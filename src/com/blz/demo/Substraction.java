c1cd6fc5775491d8944eb976e362b3491bfe3c26
pranit67 <pranit67@gmail.com>
2022-12-20 19:25:46 +0530

    Added Addition and Sustraction Program in UC1

---
+++ src/com/blz/demo/Substraction.java
@@ -0,0 +1,7 @@
+package com.blz.demo;
+
+public class Substraction {
+        public static int substact(int a,int b){
+            return (a-b);
+    }
+}
--- src/com/blz/demo/WelcomeMessage.java
+++ src/com/blz/demo/WelcomeMessage.java
@@ -3,5 +3,11 @@ package com.blz.demo;
 public class WelcomeMessage {
     public static void main(String[] args) {
         System.out.println("Welcome To Bridgelabz...!");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println(sum);
+
+        Substraction.substact(10,5);
     }
 }
